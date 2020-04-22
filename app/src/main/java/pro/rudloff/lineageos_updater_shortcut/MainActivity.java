9af3334f9188d3859746e69ba41518bcad2a5355
Pierre Rudloff <contact@rudloff.pro>
2020-04-22 20:20:06 +0200

    Catch exception when the intent fails

--- app/src/main/java/pro/rudloff/lineageos_updater_shortcut/MainActivity.java
+++ app/src/main/java/pro/rudloff/lineageos_updater_shortcut/MainActivity.java
@@ -3,6 +3,7 @@ package pro.rudloff.lineageos_updater_shortcut;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -10,8 +11,12 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Intent intent = new Intent("android.settings.SYSTEM_UPDATE_SETTINGS");
-        this.startActivity(intent);
+        try {
+            this.startActivity(new Intent("android.settings.SYSTEM_UPDATE_SETTINGS"));
+        } catch (RuntimeException exception) {
+            Toast.makeText(this, R.string.launch_error, Toast.LENGTH_SHORT).show();
+        }
+
         finish();
     }
 }
